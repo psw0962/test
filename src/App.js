c9a5ab97ebd21da9b32df42329d58ed0b2cdd2bb
Nick <65061325+psw0962@users.noreply.github.com>
2021-09-26 21:27:33 +0900

    useQuery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useQuery } from "react-query";
+
+const fetchUsers = async () => {
+  try {
+    return await (await fetch("https://reqres.in/api/users")).json();
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
 function App() {
-  return <div>asd</div>;
+  const { data: users, isLoading, isFetching, error } = useQuery("users", fetchUsers);
+  console.log(users);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Something went wrong</p>;
+  }
+
+  return (
+    <div>
+      {users.data.map((user) => (
+        <p key={user.id}>
+          {user.first_name} {user.last_name}
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
