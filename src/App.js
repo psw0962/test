09e3a6627f382e2fd303b6132f0af3ea67350207
Nick <65061325+psw0962@users.noreply.github.com>
2021-10-04 13:09:11 +0900

    useMutation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 
 const fetchUsers = async () => {
   try {
@@ -8,9 +8,33 @@ const fetchUsers = async () => {
   }
 };
 
+const addUser = async (user) => {
+  try {
+    return await (
+      await fetch("https://reqres.in/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+          first_name: user.first_name,
+          last_name: user.last_name,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+    ).json();
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
 function App() {
+  // Call all users
   const { data: users, isLoading, isFetching, error } = useQuery("users", fetchUsers);
-  console.log(users);
+
+  // Create a Mutation for adding user
+  // const {mutate, mutateAsync, isLoading: isAddingUser, error: addError} = useMutation(addUser, {
+  //   onSuccess:
+  // })
 
   if (isLoading) {
     return <p>Loading...</p>;
